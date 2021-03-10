492f1d4fedf253d3617a90403f558aaab9985a94
Bogdan Bebereche <bogdanbebereche@gmail.com>
2021-03-10 19:03:20 +0200

    Small fix after seminar

--- Week3 - Clean Code Refactoring/src/ro/ase/acs/cts/g1092/refactor/phase3/SalesStrategy.java
+++ Week3 - Clean Code Refactoring/src/ro/ase/acs/cts/g1092/refactor/phase3/SalesStrategy.java
@@ -6,50 +6,41 @@ public class SalesStrategy {
 	
 	public static final int FIDELITY_YEARS_THRESHOLD = 10;
 	public static final float MAX_FIDELITY_DISCOUNT = 0.15f;
-	public static final float NORMAL_DISCOUNT = 0.1f;
 	
 	public static float getFidelityDiscount(int yearsSinceRegistration) {
-		return (yearsSinceRegistration > FIDELITY_YEARS_THRESHOLD)
-	    		? MAX_FIDELITY_DISCOUNT : (float)yearsSinceRegistration/100; 
+		return (yearsSinceRegistration > FIDELITY_YEARS_THRESHOLD) ? MAX_FIDELITY_DISCOUNT : (float)yearsSinceRegistration/100; 
 	}
 	
 	public static float getPriceWithDiscount(float initialPrice, float discount, float fidelityDiscount) {
-		return 0;
-		//to complete
-		
-	}	
-	public float computeFinalPrice(ProductType productType,
-			float initialPrice, int yearsSinceRegistration) throws InvalidValueException
-		{
-		
-		if(initialPrice <= 0 || yearsSinceRegistration < 0) {
-			throw new InvalidValueException();
-		}
-
-		
-	    float finalPrice = 0;
-	    float fidelityDiscount = getFidelityDiscount(yearsSinceRegistration);
-	    
-	    switch(productType) {
-	    case NEW:
-	    	finalPrice = initialPrice;
-	    	break;
-	    case DISCOUNTED:
-	    	finalPrice =
-	    	(initialPrice - (ProductType.DISCOUNTED.getDiscount() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.DISCOUNTED.getDiscount() * initialPrice));
-	    	break;
-	    case LIMITED_STOCK:
-	    	finalPrice =
-	    	(initialPrice - (ProductType.LIMITED_STOCK.getDiscount() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.LIMITED_STOCK.getDiscount() * initialPrice));
-	    	break;
-	    case LEGACY:
-		    finalPrice =
-		    (initialPrice - (ProductType.LEGACY.getDiscount() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.LEGACY.getDiscount() * initialPrice));
-		    break;
-		default:
-			throw new UnsupportedOperationException("Type not managed");
-	    }
-	    
-	    return finalPrice;
-	  }
+		return (initialPrice - (discount * initialPrice)) - fidelityDiscount * (initialPrice - (discount * initialPrice));
+	}
+	
+	public float computeFinalPrice(ProductType productType, float initialPrice, int yearsSinceRegistration) throws InvalidValueException
+	{
+			if(initialPrice <= 0 || yearsSinceRegistration < 0) {
+				throw new InvalidValueException();
+			}
+			
+		    float finalPrice = 0;
+		    float fidelityDiscount = getFidelityDiscount(yearsSinceRegistration);
+		   
+		    switch(productType) {
+		    case NEW:
+			    finalPrice = initialPrice;
+			    break;
+	    	case DISCOUNTED:
+			    finalPrice = getPriceWithDiscount(initialPrice, ProductType.DISCOUNTED.getDiscount(), fidelityDiscount);
+			    break;
+	    	case LIMITED_STOCK:
+			    finalPrice = getPriceWithDiscount(initialPrice, ProductType.LIMITED_STOCK.getDiscount(), fidelityDiscount);
+			    break;
+	    	case LEGACY:
+			    finalPrice = getPriceWithDiscount(initialPrice, ProductType.LEGACY.getDiscount(), fidelityDiscount);
+			    break;
+			default:
+				throw new UnsupportedOperationException("Type not managed");
+		    }
+		    
+		    return finalPrice;
+	}
 }
