dbe0f754d0b9d299b8667c97a140d9f7ee75b93e
Bogdan Bebereche <bogdanbebereche@gmail.com>
2021-03-16 10:33:47 +0200

    Commit before break and fixed some things before

---
+++ Week3 - Clean Code Refactoring/src/ro/ase/acs/cts/g1092/refactor/phase5/SalesValidatorsInterface.java
@@ -0,0 +1,12 @@
+package ro.ase.acs.cts.g1092.refactor.phase5;
+
+import ro.ase.acs.cts.g1092.refactor.exceptions.InvalidPriceException;
+import ro.ase.acs.cts.g1092.refactor.exceptions.InvalidValueException;
+import ro.ase.acs.cts.g1092.refactor.exceptions.InvalidYearsSinceRegistrationException;
+
+public interface SalesValidatorsInterface {
+	public abstract void validatePrice(float initialPrice) throws InvalidPriceException;
+	public abstract void validateYearsSinceRegistration(int accountAge)
+			throws InvalidYearsSinceRegistrationException;
+
+}
