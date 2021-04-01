73ebb1850e1ed6411392a1547600a5bd227a4bdb
Bogdan Bebereche <bogdanbebereche@gmail.com>
2021-04-01 15:20:26 +0300

    Builder implementation

--- Week6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/builder/TestBuilder.java
+++ Week6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/builder/TestBuilder.java
@@ -1,16 +1,25 @@
 package ro.ase.csie.cts.g1093.dp.builder;
 
+import ro.ase.csie.cts.g1093.dp.builder.SuperHero.SuperHeroBuilder;
+
 public class TestBuilder {
 
 	public static void main(String[] args) {
 
 		// 1.Create the object
-		SuperHero superHero = new SuperHero();
+//		SuperHero superHero = new SuperHero();
 		// 2. Init
-
-		SuperHero superHero2 = new SuperHero("SuperMan", 100,
-				false, false, new Weapon(), null, new Flying(), null);
-
+//
+//		SuperHero superHero2 = new SuperHero("SuperMan", 100,
+//				false, false, new Weapon(), null, new Flying(), null);
+		
+		SuperHero superman = new SuperHero.SuperHeroBuilder("Superman", 0).build();
+		SuperHero joker = new SuperHero.SuperHeroBuilder("Joker", 200)
+				.isVillain()
+				.isWounded()
+				.setRightWeapon(new Weapon())
+				.build();
+		
 	}
 
 }
